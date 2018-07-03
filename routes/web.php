f796562ce25882bef43f71af774e222d802eab10
joel-okello <okellojoelacaye@gmail.com>
2018-07-03 10:15:55 +0300

    added migrations and all other files

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Auth::routes();
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+Route::get('/transactions', function () {
+    return view('transactions');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+
